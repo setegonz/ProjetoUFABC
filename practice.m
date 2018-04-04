45852d1c13e04dcd185dd8d77016f39fe216b1da
setegonz <sete.gonz@gmail.com>
2018-04-04 18:03:53 -0300

    Clock improvements

--- practice.m
+++ practice.m
@@ -8,16 +8,16 @@ Screen('Flip', window);
 
 KbWait;
 
-instructions = 'Agora vamos praticar a primeira tarefa. \n Nessas rodadas, você verá uma cruz de fixação verde se \n você está correto e uma cruz vermelha de fixação se você estiver incorreto. \n Pressione espaço para começar.';
-Screen('TextFont', window, 'Avenir');
-Screen('TextSize', window, 80);
-DrawFormattedText(window, instructions, 'center', 'center', 0, [], [], [], 1.5);
-Screen('Flip', window);
-
-[~, ~, ~] = KbWait([], 2);
+% instructions = 'Agora vamos praticar a primeira tarefa. \n Nessas rodadas, você verá uma cruz de fixação verde se \n você está correto e uma cruz vermelha de fixação se você estiver incorreto. \n Pressione espaço para começar.';
+% Screen('TextFont', window, 'Avenir');
+% Screen('TextSize', window, 80);
+% DrawFormattedText(window, instructions, 'center', 'center', 0, [], [], [], 1.5);
+% Screen('Flip', window);
+% 
+% [~, ~, ~] = KbWait([], 2);
  
 
-intro = 'Vamos praticar a tarefa one-back.\n Pressione espaço para praticar.';
+intro = 'Vamos praticar a tarefa one-back.\n Nesta tarefa você debe pressionar espaço se tem \n uma imagem duas vezes seguidas.\n Lembre-se de zerar o relógio toda vez \n que um minuto de experimento passar. \n Pressione espaço para praticar.';
 Screen('TextFont', window, 'Avenir');
 Screen('TextSize', window, 80);
 DrawFormattedText(window, intro, 'center', 'center', 0, [], [], [], 1.5);
